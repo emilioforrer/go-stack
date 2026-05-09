1eb8a08c71f24417377df00c3fc8b886701c430a
Emilio Forrer <dev.emilio.forrer@gmail.com>
2026-05-08 23:17:08 -0600

    fix: improve error handling output in main function

--- internal/scaffold/template/cmd/app/main.go
+++ internal/scaffold/template/cmd/app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"os"
 )
 
@@ -13,9 +14,11 @@ func main() {
 	if err := Execute(); err != nil {
 		var exitErr *ExitError
 		if errors.As(err, &exitErr) {
+			fmt.Fprintln(os.Stderr, exitErr.Err)
 			osExit(exitErr.Code)
 			return
 		}
+		fmt.Fprintln(os.Stderr, err)
 		osExit(1)
 	}
 }
